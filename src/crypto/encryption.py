5ad288febf42deaf7e5b00934a7899b435b7ef17
paulo <paulofvilarimda@gmail.com>
2025-02-16 14:45:20 -0300

    refactor(code): files and folder structure

--- src/crypto/encryption.py
+++ src/crypto/encryption.py
@@ -1,17 +1,9 @@
 from cryptography.hazmat.primitives.ciphers import Cipher, modes
 from cryptography.hazmat.primitives.ciphers import algorithms
-from cryptography.hazmat.primitives.ciphers.base import (
-    _CipherContext,
-    _AEADEncryptionContext,
-    _AEADCipherContext,
-)
 
 
 def get_cipher(
     key: bytes,
-) -> tuple[
-    _AEADEncryptionContext | _AEADCipherContext | _CipherContext,
-    _AEADEncryptionContext | _AEADCipherContext | _CipherContext,
-]:
+) -> tuple:
     cipher = Cipher(algorithm=algorithms.AES(key), mode=modes.CFB(b"a" * 16))
     return cipher.encryptor(), cipher.decryptor()
